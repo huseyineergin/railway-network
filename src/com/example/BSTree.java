86e24a7fa14e7d1d12f9cd60b9dedb0349559ffd
Hüseyin Emre Ergin <100281013+huseyineergin@users.noreply.github.com>
2024-03-16 12:14:33 +0300

    add BSTree class

---
+++ src/com/example/BSTree.java
@@ -0,0 +1,130 @@
+package com.example;
+
+import java.text.Collator;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+public class BSTree {
+    private int size = 0;
+    private TreeNode root;
+    private static BSTree BSTree;
+    private static Locale locale;
+    private static Collator collator;
+    private static Map<String, ArrayList<LinkedList.Station>> transferMap;
+
+    public static BSTree getInstance() {
+        if (BSTree == null) {
+            BSTree = new BSTree();
+            transferMap = new HashMap<>();
+            locale = new Locale("tr", "TR");
+            collator = Collator.getInstance(locale);
+        }
+        return BSTree;
+    }
+
+    public TreeNode findNode(String str) {
+        TreeNode curr = root;
+        str = str.trim().toLowerCase(locale);
+
+        while (curr != null) {
+            String nodeName = curr.stations.get(0).getStationName().toLowerCase(locale);
+
+            if (collator.compare(str, nodeName) < 0) {
+                curr = curr.left;
+            } else if (collator.compare(str, nodeName) > 0) {
+                curr = curr.right;
+            } else {
+                return curr;
+            }
+        }
+        return null;
+    }
+
+    public void insert(LinkedList.Station station) {
+        if (size == 0) {
+            root = new TreeNode();
+            root.stations.add(station);
+        } else {
+            TreeNode curr = root;
+            String stationName = station.getStationName();
+
+            while (curr != null) {
+                String nodeName = curr.stations.get(0).getStationName();
+
+                if (collator.compare(stationName, nodeName) < 0) {
+                    if (curr.left != null) {
+                        curr = curr.left;
+                    } else {
+                        TreeNode node = new TreeNode();
+                        node.stations.add(station);
+                        curr.left = node;
+                        break;
+                    }
+                } else if (collator.compare(stationName, nodeName) > 0) {
+                    if (curr.right != null) {
+                        curr = curr.right;
+                    } else {
+                        TreeNode node = new TreeNode();
+                        node.stations.add(station);
+                        curr.right = node;
+                        break;
+                    }
+                } else { // Same Occurrence
+                    curr.stations.add(station);
+                    break;
+                }
+            }
+        }
+        String transferPoint = station.getTransferPoint();
+
+        if (transferPoint != null) {
+            ArrayList<LinkedList.Station> transfers = transferMap.get(transferPoint);
+
+            if (transfers == null) {
+                transfers = new ArrayList<>();
+                transferMap.put(transferPoint, transfers);
+                transferMap.get(transferPoint).add(station);
+            } else {
+                transfers.add(station);
+
+                transfers.forEach(s -> {
+                    if (s != station) {
+                        s.getTransfers().add(station);
+                        station.getTransfers().add(s);
+                    }
+                });
+            }
+        }
+
+        size++;
+    }
+
+    public void inorder(TreeNode root) {
+        if (root == null)
+            return;
+        inorder(root.left);
+        root.stations.forEach(station -> System.out.println(station.getStationName() + ": " + station.getTransportLine()));
+        inorder(root.right);
+    }
+
+    public TreeNode getRoot() {
+        return root;
+    }
+
+    public static class TreeNode {
+
+        private TreeNode left;
+        private TreeNode right;
+        private final ArrayList<LinkedList.Station> stations;
+
+        public TreeNode() {
+            stations = new ArrayList<>();
+        }
+
+        public ArrayList<LinkedList.Station> getStations() {
+            return stations;
+        }
+    }
+}
